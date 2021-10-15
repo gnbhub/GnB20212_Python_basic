112a7659b18e288d9250360e89896eaaaedcd772
KimKyungYeop <81094980+KimKyungYeop@users.noreply.github.com>
2021-10-16 03:21:53 +0900

    Update kyungyeop.py

--- Week2/kyungyeop.py
+++ Week2/kyungyeop.py
@@ -10,3 +10,21 @@ while i <= num:
 print("합 : %d" % hap) 
 
 Q2
+num = 0
+num = int(input("숫자를 입력하세요.( 0 : 종료) :"))
+while True:
+    if num == 0 :
+        print("실행을 종료하겠습니다.")
+        break
+    elif num == 1:
+        print("소수가 아닙니다.")
+        break
+    else:
+        for i in range(2,num,1) :
+            if num % i == 0:
+                print("%d는(은) 소수가 아닙니다." %num)
+                break
+            else:
+                print("%d는(은) 소수입니다."%num)
+                break
+    break
