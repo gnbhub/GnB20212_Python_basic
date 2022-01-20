8c92418c0a9734d2cd908d8ba104b64c6e9ad938
Changbeom-Park <81213138+Changbeom-Park@users.noreply.github.com>
2022-01-21 08:39:23 +0900

    Update changbeom3.py

--- Week3/changbeom3.py
+++ Week3/changbeom3.py
@@ -1,5 +1,7 @@
 x=input('문자열을 입력:')
-y=x.split()
-k = len(y)
-for i in range(0, k, 1):
-  print(y[i])
+def split(x):
+  y=x.split()
+  k = len(y)
+  for i in range(0, k, 1):
+    print(y[i])
+split(x)
